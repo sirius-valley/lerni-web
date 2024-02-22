304554c5f549e0c6e4e71230caee77080fa1779a
dipietronicolas <dipietronicolas@gmail.com>
2024-02-22 14:04:51 -0300

    feature: register screen, in progress

---
+++ src/components/register/ValidationMessage/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyledRow, StyledText } from '../../styled/styles';
+import { useTheme } from 'styled-components';
+import CheckIcon from '../../../assets/icons/CheckIcon';
+import MultiplyIcon from '../../../assets/icons/MultiplyIcon';
+
+interface ValidationMessageInterface {
+  isValid: boolean;
+  message: string;
+}
+
+const ValidationMessage = ({ isValid, message }: ValidationMessageInterface) => {
+  const theme = useTheme();
+  return (
+    <StyledRow css={{ gap: '8px' }}>
+      {isValid ? <CheckIcon /> : <MultiplyIcon />}
+      <StyledText css={{ color: theme.white }}>{message}</StyledText>
+    </StyledRow>
+  );
+};
+
+export default ValidationMessage;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../screens/Home';
 import NotFound from '../screens/NotFound404';
 import Login from '../screens/Login';
+import Register from '../screens/Register';
 
 const Router = () => {
   return (
     <Routes>
-      <Route index element={<Login />} />
-      <Route element={<Home />} />
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
