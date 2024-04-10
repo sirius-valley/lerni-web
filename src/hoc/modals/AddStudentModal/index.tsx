3a99bf4dcad4029c76dd3cc10ce1133ffe5eb4c7
valenchus <valentin.morali@sirius.com.ar>
2024-04-09 20:45:40 -0700

    feature: add student modal

---
+++ src/hoc/modals/AddStudentModal/index.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+import { StyledBox, StyledColumn, StyledRow, StyledText } from '../../../components/styled/styles';
+import { ModalProps } from '../interfaces';
+import Card from '../../../components/Card';
+import CloseIcon from '../../../assets/icons/CloseIcon';
+import Button from '../../../components/styled/Button';
+import { ComponentVariantType } from '../../../utils/constants';
+import { useLDispatch, useLSelector } from '../../../redux/hooks';
+import { useVerifyStudentsMutation } from '../../../redux/service/program.service';
+import { errorToast, successToast } from '../../../components/Toasts';
+import { TextInput } from '../../../components/styled/TextInput';
+import { updatePillInfo } from '../../../redux/slices/program.slice';
+
+interface AddStudentModal extends ModalProps {
+  openModal?: boolean;
+}
+
+const AddStudentModal = ({ handleOnClose }: AddStudentModal) => {
+  const [
+    verifyStudent,
+    { isLoading: studentLoading, data: studentData, isSuccess: studentSuccess },
+  ] = useVerifyStudentsMutation();
+
+  const [studentEmail, setStudentEmail] = useState<string>('');
+
+  const dispatch = useLDispatch();
+  const students = useLSelector((state) => state.program.students);
+
+  const ValidateEmail = (mail: string) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true;
+    }
+    return false;
+  };
+  const isValidEmail = ValidateEmail(studentEmail);
+
+  const handleAddStudent = () => {
+    verifyStudent({ emails: [studentEmail] });
+  };
+
+  useEffect(() => {
+    if (studentSuccess) {
+      if (studentData) {
+        dispatch(updatePillInfo({ students: [...students, { email: studentEmail }] }));
+        successToast('Estudiante cargado con exito!');
+        handleOnClose();
+      } else {
+        errorToast('El estudiante no forma parte de LERNI');
+      }
+    }
+  }, [studentSuccess]);
+
+  const cardHeader = () => (
+    <StyledRow
+      css={{
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: '12px',
+      }}
+    >
+      <StyledColumn css={{ marginTop: '8px', gap: '12px' }}>
+        <StyledText variant="h1" css={{ fontFamily: 'Roboto-Bold' }}>
+          Agregar estudiante
+        </StyledText>
+      </StyledColumn>
+      <StyledBox onClick={() => handleOnClose()} css={{ padding: 8, cursor: 'pointer' }}>
+        <CloseIcon />
+      </StyledBox>
+    </StyledRow>
+  );
+
+  return (
+    <Card
+      headerComponent={cardHeader()}
+      onClick={(event) => {
+        event.stopPropagation();
+      }}
+      css={{
+        height: 'fit-content',
+        width: '568px',
+        zIndex: 30,
+        borderRadius: '20px',
+      }}
+    >
+      <StyledColumn css={{ width: '100%' }}>
+        <TextInput
+          placeholder="example@mail.com"
+          title="Email"
+          value={studentEmail}
+          onChange={(value) => setStudentEmail(value)}
+          maxLength={50}
+          required
+        />
+        <StyledRow
+          css={{
+            marginTop: '12px',
+            width: '100%',
+            gap: '16px',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Button
+            variant={ComponentVariantType.GHOST}
+            onClick={handleOnClose}
+            css={{
+              paddingLeft: '50px',
+              paddingRight: '50px',
+            }}
+          >
+            Cancelar
+          </Button>
+
+          <Button
+            variant={ComponentVariantType.PRIMARY}
+            onClick={handleAddStudent}
+            disabled={studentLoading || !isValidEmail}
+            css={{
+              paddingLeft: '50px',
+              paddingRight: '50px',
+            }}
+          >
+            Agregar
+          </Button>
+        </StyledRow>
+      </StyledColumn>
+    </Card>
+  );
+};
+
+export default AddStudentModal;
