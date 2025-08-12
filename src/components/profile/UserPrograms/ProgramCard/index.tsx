2828aeab89239bf5718aea938c313544a527f68e
lizlubelczyk <llubelczyk@mail.austral.edu.ar>
2025-08-12 14:32:40 -0300

    student profile

--- src/components/profile/UserPrograms/ProgramCard/index.tsx
+++ src/components/profile/UserPrograms/ProgramCard/index.tsx
@@ -9,7 +9,6 @@ import {
 } from '../../../styled/styles';
 import { useTheme } from 'styled-components';
 import CheckIcon from '../../../../assets/icons/CheckIcon';
-import { Link } from 'react-router-dom';
 
 interface ProgramCardProps {
   program: ProgramCardItem;
@@ -19,104 +18,119 @@ const ProgramCard: React.FC<ProgramCardProps> = ({ program }) => {
   const theme = useTheme();
 
   return (
-    <Link to={`/program/${program.id}`} style={{ textDecoration: 'none' }}>
-      <StyledColumn
-        style={{
-          backgroundColor: theme.gray100,
-          borderRadius: 8,
-          overflow: 'hidden',
-          width: '240px',
-          position: 'relative',
-        }}
-      >
-        {program.progress < 100 ? (
-          <StyledBox
+    <StyledColumn
+      style={{
+        backgroundColor: theme.gray100,
+        borderRadius: 8,
+        overflow: 'hidden',
+        width: '240px',
+        position: 'relative',
+      }}
+    >
+      {program.progress < 100 ? (
+        <StyledBox
+          style={{
+            position: 'absolute',
+            top: '125px',
+            left: '90%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: theme.gray400,
+            padding: '5px 8px',
+            borderRadius: 20,
+          }}
+        >
+          <StyledText style={{ fontFamily: 'Roboto-Bold', fontSize: '12px' }} color="white">
+            {program.progress}%
+          </StyledText>
+        </StyledBox>
+      ) : (
+        <StyledColumn
+          style={{
+            position: 'absolute',
+            top: '12px',
+            right: '12px',
+            gap: '5px',
+            alignItems: 'flex-end',
+          }}
+        >
+          <StyledRow
             style={{
-              position: 'absolute',
-              top: '125px',
-              left: '90%',
-              transform: 'translate(-50%, -50%)',
-              backgroundColor: theme.gray400,
+              backgroundColor: theme.primary500,
               padding: '5px 8px',
               borderRadius: 20,
+              width: 'fit-content',
+              alignItems: 'center',
+              gap: 4,
+            }}
+          >
+            <CheckIcon />
+            <StyledText style={{ fontFamily: 'Roboto-Bold', fontSize: '12px' }} color="white">
+              Completado
+            </StyledText>
+          </StyledRow>
+
+          <StyledBox
+            style={{
+              backgroundColor: theme.primary500,
+              padding: '5px 8px',
+              borderRadius: 20,
+              width: 'fit-content',
             }}
           >
             <StyledText style={{ fontFamily: 'Roboto-Bold', fontSize: '12px' }} color="white">
-              {program.progress}%
+              {program.points}/{program.maxPoints}
             </StyledText>
           </StyledBox>
-        ) : (
-          <StyledColumn
-            style={{
-              position: 'absolute',
-              top: '12px',
-              right: '12px',
-              gap: '5px',
-              alignItems: 'flex-end',
-            }}
-          >
-            <StyledRow
-              style={{
-                backgroundColor: theme.primary500,
-                padding: '5px 8px',
-                borderRadius: 20,
-                width: 'fit-content',
-                alignItems: 'center',
-                gap: 4,
-              }}
-            >
-              <CheckIcon />
-              <StyledText style={{ fontFamily: 'Roboto-Bold', fontSize: '12px' }} color="white">
-                Completado
-              </StyledText>
-            </StyledRow>
+        </StyledColumn>
+      )}
 
-            <StyledBox
-              style={{
-                backgroundColor: theme.primary500,
-                padding: '5px 8px',
-                borderRadius: 20,
-                width: 'fit-content',
-              }}
-            >
-              <StyledText style={{ fontFamily: 'Roboto-Bold', fontSize: '12px' }} color="white">
-                {program.points}/{program.maxPoints}
-              </StyledText>
-            </StyledBox>
-          </StyledColumn>
-        )}
+      <StyledImage src={program.image} style={{ height: 125, width: '100%', objectFit: 'cover' }} />
 
-        <StyledImage
-          src={program.image}
-          style={{ height: 125, width: '100%', objectFit: 'cover' }}
-        />
-
-        <StyledColumn style={{ padding: 16, gap: 16 }}>
+      <StyledColumn
+        style={{ padding: 16, gap: 16, minHeight: '120px', justifyContent: 'space-between' }}
+      >
+        <StyledColumn style={{ gap: 8 }}>
           <StyledText variant="h4" css={{ fontSize: '16px' }}>
             {program.name}
           </StyledText>
 
-          <StyledRow style={{ gap: 4 }}>
-            <StyledImage
-              src={program.teacher.image}
-              css={{
-                borderRadius: '100%',
-                width: '28px',
-                height: '28px',
-                objectFit: 'cover',
-              }}
-            />
+          {program.collection ? (
             <StyledText
               variant="body2"
-              css={{ fontFamily: 'Roboto', fontSize: '14px', alignContent: 'center' }}
-              color="gray400"
+              css={{
+                fontFamily: 'Roboto',
+                fontSize: '12px',
+                color: theme.gray400,
+                fontStyle: 'italic',
+              }}
             >
-              {program.teacher.name} {program.teacher.lastname}
+              {program.collection}
             </StyledText>
-          </StyledRow>
+          ) : (
+            <div style={{ height: '16px' }}></div>
+          )}
         </StyledColumn>
+
+        <StyledRow style={{ gap: 4 }}>
+          <StyledImage
+            src={program.teacher.image}
+            css={{
+              borderRadius: '100%',
+              width: '28px',
+              height: '28px',
+              objectFit: 'cover',
+            }}
+          />
+          <StyledText
+            variant="body2"
+            css={{ fontFamily: 'Roboto', fontSize: '14px', alignContent: 'center' }}
+            color="gray400"
+          >
+            {program.teacher.name} {program.teacher.lastname}
+          </StyledText>
+        </StyledRow>
       </StyledColumn>
-    </Link>
+    </StyledColumn>
   );
 };
 
